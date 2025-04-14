30bed78e03b96d8b1d402f9346c010e0e86fa9ac
Simon Scabello <simon.scabello@live.com>
2025-04-14 18:15:08 -0300

    Ajusta logs.

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"url-shortener/internal/handlers"
 	"url-shortener/internal/storage"
@@ -10,19 +9,12 @@ import (
 )
 
 func main() {
-	fmt.Println("🔍 Variáveis de ambiente disponíveis:")
-	for _, e := range os.Environ() {
-		fmt.Println(e)
-	}
-
 	app := fiber.New()
 
 	redisURL := os.Getenv("REDIS_URL")
 	if redisURL == "" {
 		panic("REDIS_URL não está definida no ambiente!")
 	}
-
-	fmt.Println("REDIS_URL:", redisURL)
 
 	store := storage.NewRedisStorage()
 
